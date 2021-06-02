5ec3032705e0157b26e4c56ff86472896df40bfd
Logan <lurfer@email.arizona.edu>
2021-06-01 21:53:25 -0700

    Started implementation of nearest neighbor algorithm.

---
+++ src/algorithms/NearestNeighbor.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+import path.Path;
+import algorithms.Algorithm;
+
+public class NearestNeighbor extends Algorithm {
+    public NearestNeighbor(Path path) {
+        super(path);
+    }
+
+    public void solve() {
+        Path temp = this.getPath();
+    }
+
+
+}
