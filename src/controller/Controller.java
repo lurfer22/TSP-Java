722d82ebd5fd250c2524a9530bcb5048c50f49c8
Logan <lurfer@email.arizona.edu>
2021-06-01 21:08:26 -0700

    Implemented Controller class.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,55 @@
 package controller;
 
+import algoname.AlgorithmName;
+import city.City;
+import model.Model;
+import path.Path;
+import java.util.List;
+
 public class Controller {
+    private Model model;
+
+    public Controller(Model model) {
+        this.model = model;
+    }
+
+    public void setNumberOfCities(int newAmount) {
+        try {
+            this.model.setNumberOfCities(newAmount);
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<City> getCities() {
+        return this.model.getCities();
+    }
+
+    public Path getSolvedPath() {
+        Path path = this.model.getSolvedPath();
+        if (path == null)
+            System.out.println("Must solve for the path!");
+        return path;
+    }
+
+    public void setUpperBounds(int x, int y) {
+        try {
+            this.model.setUpperBounds(x, y);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean setStartingCity(City city) {
+        boolean check = this.model.setStartingCity(city);
+        if (!check)
+            System.out.println("Trying to set a starting city that " +
+                                "is not included in the available cities.");
+
+        return check;
+    }
+
+    public void solve(AlgorithmName type) {
+        this.model.solvePath(type);
+    }
 }
